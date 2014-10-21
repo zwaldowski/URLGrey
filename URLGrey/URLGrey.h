b3d4a7ed4442cf9dbd075ac470ff9521c261db69
Zachary Waldowski <zach@waldowski.me>
2014-10-21 11:57:09 -0400

    Initial Commit

---
+++ URLGrey/URLGrey.h
@@ -0,0 +1,19 @@
+//
+//  URLGrey.h
+//  URLGrey
+//
+//  Created by Zachary Waldowski on 10/21/14.
+//  Copyright (c) 2014 Zachary Waldowski. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for URLGrey.
+FOUNDATION_EXPORT double URLGreyVersionNumber;
+
+//! Project version string for URLGrey.
+FOUNDATION_EXPORT const unsigned char URLGreyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLGrey/PublicHeader.h>
+
+
