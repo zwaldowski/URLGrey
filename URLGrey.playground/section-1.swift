1a171ee3640f5384ade9475366cb0918520eea77
Zachary Waldowski <zach@waldowski.me>
2014-12-11 01:38:55 -0500

    Add a workspace playground

---
+++ URLGrey.playground/section-1.swift
@@ -0,0 +1,7 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+import URLGrey
+
+let swift = UTI(preferredTag: .FilenameExtension("swift"))
+let bundle = swift.declaringBundleURL
