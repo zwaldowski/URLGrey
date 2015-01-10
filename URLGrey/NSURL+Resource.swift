bc4b23ab8436b7f41ba7416c4b78e44305591ead
Zachary Waldowski <zach@waldowski.me>
2015-01-10 12:42:31 -0500

    Give errors localized failure reasons

--- URLGrey/NSURL+Resource.swift
+++ URLGrey/NSURL+Resource.swift
@@ -12,32 +12,38 @@ import LlamaKit
 public extension NSURL {
     
     public func getResourceValue<K, V where K: ReadableResource, V == K.ValueType>(forKey key: K) -> Result<V> {
+        let keyString = key.stringValue
+
         var value: AnyObject?
         var fetchError: NSError?
-        if (getResourceValue(&value, forKey: key.stringValue, error: &fetchError)) {
+        if (getResourceValue(&value, forKey: keyString, error: &fetchError)) {
             if let inValue = value as? K.InputType {
                 if let ret = key.read(inValue) {
                     return success(ret)
                 }
             }
-            return failure(error(code: URLGreyError.InvalidResourceValue(key.stringValue)))
+            
+            return failure(error(code: URLError.InvalidResourceRead(keyString)))
         } else {
-            return failure(fetchError!)
+            return failure(error(code: URLError.ReadResource(keyString), underlying: fetchError))
         }
     }
     
     public func setResourceValue<K, V where K: WritableResource, V == K.ValueType, K.OriginalType: AnyObject>(value: V?, forKey key: K) -> Result<()> {
-        var error: NSError?
-        var finalValue: AnyObject?
+        let keyString = key.stringValue
         
-        if let value = value {
-            finalValue = key.write(value) as? K.OriginalType
-        }
-        
-        if self.setResourceValue(finalValue, forKey: key.stringValue, error: &error) {
-            return success()
+        // This is ugly, but `let`-ing it out separately got it mad
+        if let finalValue: AnyObject? = value.map({
+            key.write($0) as? K.OriginalType
+        }) {
+            var saveError: NSError?
+            if setResourceValue(finalValue, forKey: key.stringValue, error: &saveError) {
+                return success()
+            } else {
+                return failure(error(code: URLError.WriteResource(keyString), underlying: saveError))
+            }
         } else {
-            return failure(error!)
+            return failure(error(code: URLError.InvalidResourceWrite(keyString)))
         }
     }
     
--- URLGrey/URLError.swift
+++ URLGrey/URLError.swift
@@ -6,23 +6,40 @@
 //  Copyright (c) 2015 Zachary Waldowski. All rights reserved.
 //
 
-public enum URLGreyError: ErrorRepresentable {
-    case InvalidResourceValue(String) // we couldn't coerce the value for key to the correct type
+public enum URLError: ErrorRepresentable {
+    case ReadResource(String) // getting a resource failed
+    case InvalidResourceRead(String) // we couldn't coerce the value for key to the correct type
+    case WriteResource(String) // setting a resource failed
+    case InvalidResourceWrite(String) // we couldn't coerce the value for writing to key to the correct type
     
     public static var domain: String {
-        return "me.waldowski.URLGrey.error"
+        return "me.waldowski.URLGrey"
     }
     
     public var code: Int {
         switch self {
-        case .InvalidResourceValue: return -1000
+        case .ReadResource: return -1000
+        case .InvalidResourceRead: return -1001
+        case .WriteResource: return -1002
+        case .InvalidResourceWrite: return -1003
         }
     }
     
-    public var localizedDescription: String? {
+    public var description: String {
+        return URLGrey.localizedString("A URL activity failed", comment: "URL error description")
+    }
+    
+    public var failureReason: String? {
         switch self {
-        case .InvalidResourceValue(let key):
-            return "Retrieving a value for \(key) succeeded, but the value was invalid."
+        case .ReadResource(let key):
+            return URLGrey.localizedString("Retrieving a value for \(key) failed", comment: "URL error reason")
+        case .InvalidResourceRead(let key):
+            return URLGrey.localizedString("Retrieving a value for \(key) succeeded but the value was invalid", comment: "URL error reason")
+        case .WriteResource(let key):
+            return URLGrey.localizedString("Committing a value for \(key) failed", comment: "URL error reason")
+        case .InvalidResourceWrite(let key):
+            return URLGrey.localizedString("The value for writing to \(key) was invalid", comment: "URL error reason")
         }
     }
+        
 }
