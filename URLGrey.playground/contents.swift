045b04101ea5c2e0294fce2943d78616743c8dd2
Zachary Waldowski <zach@waldowski.me>
2015-03-27 18:11:15 -0400

    Upgrade and fix playground

--- URLGrey.playground/contents.swift
+++ URLGrey.playground/contents.swift
@@ -3,5 +3,5 @@
 import URLGrey
 
 let url = NSURL(fileURLWithPath: "/System/Library/CoreServices/SystemVersion.plist", isDirectory: false)!
-let uti = url.value(forResource: Resource.TypeIdentifier).value!
+let uti = url.value(forResource: Resource.TypeIdentifier).value
 let myUTI = UTI(preferredTag: .FilenameExtension("plist"))
