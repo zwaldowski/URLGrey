fa532b0789ae4512b8913fce9fccfbf132deb900
Zachary Waldowski <zach@waldowski.me>
2015-07-27 01:33:01 -0400

    Update playground for new syntax

--- URLGrey.playground/contents.swift
+++ URLGrey.playground/contents.swift
@@ -1,23 +1,22 @@
 // Playground - noun: a place where people can play
 
 import URLGrey
-import Lustre
 import XCPlayground
 
 let fm = NSFileManager()
-let sysLib = fm.URLForDirectory(.LibraryDirectory, inDomain: .SystemDomainMask, appropriateForURL: nil, create: false, error: nil)!
+let sysLib = try! fm.URLForDirectory(.LibraryDirectory, inDomain: .SystemDomainMask, appropriateForURL: nil, create: false)
 let coreSvcs = sysLib + .Directory("CoreServices")
 let sysVersion = coreSvcs + .File("SystemVersion", .PropertyList)
 
-let uti = sysVersion.value(forResource: .TypeIdentifier).value
+let uti = try! sysVersion.valueForResource(URLResource.TypeIdentifier)
 let myUTI = UTI(preferredTag: .FilenameExtension("plist"))
 
-for url in coreSvcs.contents(fetchedResources: [ .TypeIdentifier ], options: .SkipsSubdirectoryDescendants) {
-    println(url)
+for url in coreSvcs.contents(fetchResources: [ URLResource.TypeIdentifier ], options: .SkipsSubdirectoryDescendants) {
+    print(url)
 }
 
 for i in sysVersion.ancestors {
-    println(i)
+    print(i)
 }
 
-let gen = sysVersion.value(forResource: .GenerationIdentifier).value
+let gen = try! sysVersion.valueForResource(URLResource.GenerationIdentifier)
