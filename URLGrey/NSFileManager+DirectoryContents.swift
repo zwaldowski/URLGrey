462ad83e269dba776552ed01dee9d9a7e43d3708
Zachary Waldowski <zach@waldowski.me>
2015-09-20 21:58:26 -0400

    Clean up file manager block in Swift 2

--- URLGrey/NSFileManager+DirectoryContents.swift
+++ URLGrey/NSFileManager+DirectoryContents.swift
@@ -14,11 +14,7 @@ extension NSFileManager {
     
     public func directory(URL url: NSURL, fetchResources: [URLResourceType] = [], options mask: NSDirectoryEnumerationOptions = [], errorHandler handler: ((NSURL, NSError) -> Bool)? = nil) -> AnySequence<NSURL> {
         let keyStrings = fetchResources.map { $0.key }
-        let unsafeHandler = handler.map { block -> (NSURL!, NSError!) -> Bool in
-            { block($0, $1) }
-        }
-                
-        if let enumerator = enumeratorAtURL(url, includingPropertiesForKeys: keyStrings, options: mask, errorHandler: unsafeHandler) {
+        if let enumerator = enumeratorAtURL(url, includingPropertiesForKeys: keyStrings, options: mask, errorHandler: handler) {
             return AnySequence(enumerator.lazy.map(unsafeDowncast))
         } else {
             return AnySequence(EmptyGenerator())
